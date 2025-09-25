9d80951f4fcb0ef7ad6899ba6ab0c8ef4afbb2ad
Nanami Nakano <64841155+NanamiNakano@users.noreply.github.com>
2025-09-25 21:04:05 +0800

    feat: setting singleton

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,12 +1,40 @@
 mod player;
+mod config;
 
+use godot::classes::Engine;
 use godot::prelude::*;
+use crate::config::Setting;
 
 struct Cave;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for Cave {
     fn on_level_init(level: InitLevel) {
-        godot_print!("Rust: Inited on {:?}", level)
+        godot_print!("Rust: Inited on {:?}", level);
+        if level == InitLevel::Scene {
+            godot_print!("Registering settings singleton");
+            Engine::singleton().register_singleton(
+                &Setting::class_name().to_string_name(),
+                &Setting::new_alloc(),
+            );
+        }
+    }
+
+    fn on_level_deinit(level: InitLevel) {
+        godot_print!("Rust: Deinited on {:?}", level);
+        if level == InitLevel::Scene {
+            let mut engine = Engine::singleton();
+            let singleton_name = &Setting::class_name().to_string_name();
+
+            if let Some(my_singleton) = engine.get_singleton(singleton_name) {
+                // Unregistering from Godot, and freeing from memory is required
+                // to avoid memory leaks, warnings, and hot reloading problems.
+                godot_print!("Unregistering settings singleton");
+                engine.unregister_singleton(singleton_name);
+                my_singleton.free();
+            } else {
+                godot_error!("Failed to get singleton");
+            }
+        }
     }
 }
---
+++ rust/src/setting.rs
@@ -0,0 +1,83 @@
+use godot::classes::ConfigFile;
+use godot::prelude::*;
+use snafu::{OptionExt, ResultExt, Snafu};
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Error when saving config"))]
+    SaveError,
+    #[snafu(display("Error when loading config"))]
+    LoadError,
+    #[snafu(display("Error when loading config: converting failed"))]
+    ConvertError { source: ConvertError },
+    #[snafu(display("Error when getting singleton"))]
+    SingletonError,
+}
+
+#[derive(GodotClass)]
+#[class(base=Object)]
+pub struct Setting {
+    inner: Gd<ConfigFile>,
+    base: Base<Object>,
+}
+
+fn load_config() -> Result<Gd<ConfigFile>, Error> {
+    let mut inner = ConfigFile::new_gd();
+    let error = inner.load("user://settings.cfg");
+    match error {
+        godot::global::Error::OK => Ok(inner),
+        _ => Err(Error::LoadError),
+    }
+}
+
+fn default_config() -> Gd<ConfigFile> {
+    let mut inner = ConfigFile::new_gd();
+    inner.set_value("global", "sensitivity", &Variant::from(0.01));
+    inner
+}
+
+#[godot_api]
+impl Setting {
+    pub fn save(&mut self) -> Result<(), Error> {
+        let error = self.inner.save("user://settings.cfg");
+        match error {
+            godot::global::Error::OK => Ok(()),
+            _ => Err(Error::SaveError),
+        }
+    }
+
+    pub fn set_value(&mut self, section: &str, key: &str, value: impl ToGodot) {
+        self.inner.set_value(section, key, &Variant::from(value));
+    }
+
+    pub fn set_and_save<T: ToGodot>(
+        &mut self,
+        section: &str,
+        key: &str,
+        value: T,
+    ) -> Result<(), Error> {
+        self.set_value(section, key, value);
+        self.save()
+    }
+
+    pub fn get_value<T: FromGodot>(&self, section: &str, key: &str) -> Result<T, Error> {
+        let variant = self.inner.get_value(section, key);
+        let value = variant.try_to_relaxed().context(ConvertSnafu)?;
+        Ok(value)
+    }
+
+    pub fn singleton() -> Result<Gd<Self>, Error> {
+        let object = godot::classes::Engine::singleton().get_singleton("Setting").context(SingletonSnafu)?;
+        Ok(object.cast())
+    }
+}
+
+#[godot_api]
+impl IObject for Setting {
+    fn init(base: Base<Self::Base>) -> Self {
+        match load_config() {
+            Ok(inner) => Self { inner, base },
+            Err(_) => Self { inner: default_config(), base },
+        }
+    }
+}
